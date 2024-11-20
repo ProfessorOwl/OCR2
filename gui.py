7012af7ae2f09077991a52fc99900b930c047491
ProfessorOwl <clemenszirbes@gmail.com>
2024-11-20 16:38:53 +0100

    GUI und init.py arbeiten zusammen

--- gui.py
+++ gui.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter.filedialog import askdirectory, asksaveasfilename
 import customtkinter as ctk
-from init import mult_ocr
+import init as ocr
 
 root = ctk.CTk()
 root.geometry("600x300")
@@ -12,7 +12,9 @@ def select_folder():
     if not folder_path == "": # Delete the entry line if "Ask Directory" window gets closed
         entry_selectfolder.delete(0,tk.END)
     entry_selectfolder.insert(0,folder_path)
-    
+    fromimage.set(ocr.file_from_to(folder_path)[0])
+    toimage.set(ocr.file_from_to(folder_path)[1])
+
 def save_where():
     file_path = asksaveasfilename(
         confirmoverwrite=False,
@@ -64,20 +66,22 @@ btn_savefile = ctk.CTkButton(
     command=save_where
 )
 entry_savefile = ctk.CTkEntry(
-    master=frame_selectsave
+    master=frame_selectsave,
 )
 btn_savefile.grid(row=1, column=0, padx=5, pady=10)
 entry_savefile.grid(row=1, column=1, sticky="ew", padx=5, pady=10)
 #----
 
 #---- Construct the Image Number entry fields
+fromimage = ctk.StringVar()
+toimage = ctk.StringVar()
 frame_imagenumbers = ctk.CTkFrame(
     master=frame_selectsave
 )
 entry_fromimagenumber = ctk.CTkEntry(
     master=frame_imagenumbers,
-    placeholder_text="First Number",
-    width=100
+    width=100,
+    textvariable=fromimage
 )
 label_betweennumbers = ctk.CTkLabel(
     master=frame_imagenumbers,
@@ -86,8 +90,8 @@ label_betweennumbers = ctk.CTkLabel(
 )
 entry_toimagenumber = ctk.CTkEntry(
     master=frame_imagenumbers,
-    placeholder_text="Last Number",
-    width=100
+    width=100,
+    textvariable=toimage
 )
 frame_imagenumbers.grid(row=1, column=2, ipady=10, ipadx=20)
 entry_fromimagenumber.grid(row=0, column=0)
@@ -109,7 +113,9 @@ entry_sheet_name = ctk.CTkEntry(
 )
 switch_sheet_name = ctk.CTkSwitch(
     master=frame_selectsave,
-    text="Overwrite existing sheet?"
+    text="Overwrite existing sheet?",
+    onvalue="replace",
+    offvalue="new"
 )
 label_sheet_name.grid(row=2, column=0, padx=5, pady=10)
 entry_sheet_name.grid(row=2, column=1, sticky="ew", padx=5, pady=10)
@@ -122,11 +128,20 @@ frame_convertbar = ctk.CTkFrame(
 )
 btn_convert = ctk.CTkButton(
     master=frame_convertbar,
-    text="Convert images"
+    text="Convert images",
+    command=lambda: ocr.mult_ocr(int(fromimage.get()),
+                                 int(toimage.get()), 
+                                 entry_savefile.get(), 
+                                 imagepath=entry_selectfolder.get(),
+                                 sheetname=entry_sheet_name.get(),
+                                 sheet_exists_toggle=switch_sheet_name.get(),
+                                 progressbar=progbar,
+                                 console=text_output)
 )
 progbar = ctk.CTkProgressBar(
-    master=frame_convertbar 
+    master=frame_convertbar
 )
+progbar.set(0)
 btn_convert.grid(row=0, column=0, padx=5, pady=10)
 progbar.grid(row=0, column=1, sticky="ew", padx=5, pady=10)
 frame_convertbar.columnconfigure(1, weight=1)
@@ -135,7 +150,7 @@ frame_convertbar.columnconfigure(1, weight=1)
 #---- Text output on the bottom of the page
 text_output = ctk.CTkTextbox(
     master=root,
-    state=tk.DISABLED
+    state="disabled"
 )
 #----
 
--- init.py
+++ init.py
@@ -5,13 +5,14 @@ import re
 import pandas as pd
 import openpyxl
 from os import listdir
-from os.path import isfile, join
+from os.path import isfile, join, exists
 
 # set correct path for pytesseract
 pytesseract.pytesseract.tesseract_cmd = r'C:\Users\czirbes\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
 
 # load image
 def path_to_image(number, path):
+    number = "{:06d}".format(number)
     img = cv2.imread(f"{path}/Volume_{number}.jpg")
     return img
 
@@ -23,22 +24,31 @@ def ocr_core(img):
 # function that automatically detects the image numbers "from" and "to"
 
 def file_from_to(path):
-    filenames = [f for f in listdir(path) if isfile(join(path, f))]
-    filenames_cleared = [name.split("_")[1] for name in filenames if name.split("_")[0] == "Volume"]
-    filenames = None
     filenames_cleared = None
-    return filenames_cleared[0].split(".")[0], filenames_cleared[-1].split(".")[0]
+    filenames = [f for f in listdir(path) if isfile(join(path, f))] # get every name of file-types in the folder
+    filenames_cleared = [name.split("_")[1] for name in filenames if name.split("_")[0] == "Volume"] # Select only the number if the file is named "Volume"
+    filenames = None # clear filenames to save space
+    return filenames_cleared[0].split(".")[0], filenames_cleared[-1].split(".")[0] # return the first and last filename
+
 
 # function that is tailored to the output of the DoD-printer software. 
-def mult_ocr(from_number, to_number, filename, sheetname="data", path="Bilder/"):
+def mult_ocr(from_number, 
+             to_number, 
+             path, 
+             sheetname="data", 
+             imagepath="Bilder/", 
+             sheet_exists_toggle="new", 
+             progressbar = None,
+             console = None):
     """Iterates through multiple files and prints the ocr output into an excel file.
     
     positional arguments:
     from_number -- from which number to start counting the files
     to_number -- to which number to count the files
-    filename -- name of excel file to be created
+    path -- name and path of excel file to be created
     sheetname -- name of excel sheet in the file
-    path -- path where the images are stored 
+    imagepath -- path where the images are stored
+    sheet_exists_toggle -- Sets which object should switch the overwrite mode of existing sheets. Standard: "new"
     """
     
     list = [] 
@@ -46,15 +56,19 @@ def mult_ocr(from_number, to_number, filename, sheetname="data", path="Bilder/")
         
         # ---- Prints the percentage of completed pictures ----
         num_of_images = to_number+1 - from_number
-        progress = round(((number-from_number)/num_of_images)*100, 1) # Rounds to the first digit after comma
-        print(f"Progress: {progress}%") # Print the percentage
-        LINE_UP = '\033[1A' # ASCII code to go to the previous line
-        LINE_CLEAR = '\x1b[2K' # ASCII code to clear the current line
-        print(LINE_UP, end=LINE_CLEAR) # GO a line up and clear after printing
+        progress = (number-from_number)/num_of_images # Rounds to the first digit after comma
+        if progressbar == None:
+            print(f"Progress: {progress}%") # Print the percentage
+            LINE_UP = '\033[1A' # ASCII code to go to the previous line
+            LINE_CLEAR = '\x1b[2K' # ASCII code to clear the current line
+            print(LINE_UP, end=LINE_CLEAR) # Go a line up and clear after printing
+        else:
+            progressbar.set(progress)
+            progressbar.update_idletasks()
         # --------
         
         # ---- Do some image processing
-        img = path_to_image(number, path)[0:40, 10:125] # Get image and crop it to the desired size to reduce computing time
+        img = path_to_image(number, imagepath)[0:40, 10:125] # Get image and crop it to the desired size to reduce computing time
         text = ocr_core(img) # Get the text via OCR
         while True:
             try:
@@ -65,19 +79,19 @@ def mult_ocr(from_number, to_number, filename, sheetname="data", path="Bilder/")
                 value = None
                 break
         converted = [number, value] # Create list and put image in the first slot. Second slot 
-                                                                       # is the OCR'ed text, which is scanned for all digits. Since both digits
-                                                                       # are separated by a dot, this has to be added via the "join" function
+                                    # is the OCR'ed text, which is scanned for all digits. Since both digits
+                                    # are separated by a dot, this has to be added via the "join" function
         list.append(converted) # Add the converted element to our list 
     df = pd.DataFrame(list) # Convert the list into a dataframe for export to excel
     df.columns=["filename", "volume / nL"] # Name the columns
-    with pd.ExcelWriter(f"{filename}.xlsx", mode="a") as writer: # Uses the "write" function, so that a new sheet can be added to an existing excel file        
-          df.to_excel(writer, sheet_name=f"{sheetname}") # Defines, what the sheet should be named as
-    print(f"Converted {abs(from_number-to_number)} images.") # Prints how many images where converted
+    if exists(path):
+        with pd.ExcelWriter(path, mode="a", if_sheet_exists=sheet_exists_toggle) as writer: # Uses the "append" function, so that a new sheet can be added to an existing excel file        
+            df.to_excel(writer, sheet_name=f"{sheetname}") # Defines, what the sheet should be named as
+    else:
+        with pd.ExcelWriter(path, mode="w") as writer: # Uses the "write" function, so that a file will be created
+            df.to_excel(writer, sheet_name=f"{sheetname}") 
+    if console == None:
+        print(f"Converted {abs(from_number-to_number)} images.") # Prints how many images where converted
+    else:
+        console.insert("0.0", f"Converted {abs(from_number-to_number)} images to sheet {sheetname} in {path}.")
     return
-
-
-#mult_ocr(6624, # First number
-#        7565, # Last number
-#        "a_PJ200 L", # Name of Excel file
-#        "n_Stroke 0 Velocity 85 1Hz", # Name of Sheet in Excel file, no special symbols allowed!
-#        "Bilder/a_PJ200 L/n_Stroke 0 Velocity 85 1Hz 2,46ms") # Path to the folder with all the pictures
